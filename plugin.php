d310e8e81a92dd0a5798b68778e8cc5ce14d72e4
kyokutyo <haruta@kyokutyo.com>
2012-12-05 02:31:35 +0900

    php-cs-fixer

--- plugin.php
+++ plugin.php
@@ -9,47 +9,53 @@ Author URI: http://kyokutyo.com/
 Author Email: kyokutyo@gmail.com
 */
 
-class Show_your_Github_activities extends WP_Widget {
+class Show_your_Github_activities extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct(
+            false,
+            'Show Your Github Activities',
+            array(
+                'description' => 'Generate widget to show your activities for lazy good neighbors.',
+            )
+        );
+    }
 
-	public function __construct() {
-		parent::__construct(
-			false,
-			'Show Your Github Activities',
-			array(
-				'description' => 'Generate widget to show your activities for lazy good neighbors.',
-			)
-		);
-	}
+    public function widget($args, $instance)
+    {
+        extract($instance, EXTR_SKIP);
+        echo $before_widget;
+        include plugin_dir_path(__FILE__) . 'view.php';
+        echo $after_widget;
+    }
 
-	public function widget($args, $instance) {
-		extract($instance, EXTR_SKIP);
-		echo $before_widget;
-		include plugin_dir_path(__FILE__) . 'view.php';
-		echo $after_widget;
-	}
+    public function update($new_instance, $old_instance)
+    {
+        $instance = $old_instance;
+        if (isset($new_instance['username'])) {
+            $instance['username'] = strip_tags($new_instance['username']);
+        }
+        if (isset($new_instance['width'])) {
+            $instance['width'] = strip_tags($new_instance['width']);
+        }
+        if (isset($new_instance['height'])) {
+            $instance['height'] = strip_tags($new_instance['height']);
+        }
 
-	public function update($new_instance, $old_instance) {
-		$instance = $old_instance;
-		if(isset($new_instance['username'])) {
-			$instance['username'] = strip_tags($new_instance['username']);
-		}
-		if(isset($new_instance['width'])) {
-			$instance['width'] = strip_tags($new_instance['width']);
-		}
-		if(isset($new_instance['height'])) {
-			$instance['height'] = strip_tags($new_instance['height']);
-		}
-		return $instance;
-	}
+        return $instance;
+    }
 
-	public function form($instance) {
-		extract($instance);
-		include plugin_dir_path( __FILE__ ) . 'option.php';
-	}
+    public function form($instance)
+    {
+        extract($instance);
+        include plugin_dir_path( __FILE__ ) . 'option.php';
+    }
 
 }
 
 add_action('widgets_init', 'register_syga_widget');
-function register_syga_widget() {
-	register_widget('Show_your_Github_activities');
+function register_syga_widget()
+{
+    register_widget('Show_your_Github_activities');
 }
